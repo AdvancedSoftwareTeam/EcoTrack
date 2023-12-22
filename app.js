6fff96c3e7daf7e2e672bcad7bce90055c7c8a6b
AyaMAwwad <149961665+AyaMAwwad@users.noreply.github.com>
2023-12-22 18:51:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,25 +5,25 @@ const sessionConfig = require('./middlewares/sessionConfig'); // Import the sess
 const app = express();
 app.use(express.json());
 app.use(sessionConfig); // Apply the session configuration
-
+//
 // Define all routers
 const userRouter = require('./routes/userRoutes');
-// const dataRouter = require('./routes/dataRoutes');
+const dataRouter = require('./routes/dataRoutes');
 // const alertsRouter = require('./routes/alertsRoutes');
 // const reportsRouter = require('./routes/reportsRoutes');
 // const scoresRouter = require('./routes/scoresRoutes');
 // const resourcesRouter = require('./routes/resourcesRoutes');
-// const openDataRouter = require('./routes/open-dataRoutes');
+const openDataRouter = require('./routes/open-dataRoutes');
 // const externalAPIRouter = require('./routes/external-apiRoutes');
 
 // Use the routers
 app.use('/api/users', userRouter);
-// app.use('/api/data', dataRouter);
+app.use('/api/data', dataRouter);
 // app.use('/api/alerts', alertsRouter);
 // app.use('/api/reports', reportsRouter);
 // app.use('/api/scores', scoresRouter);
 // app.use('/api/resources', resourcesRouter);
-// app.use('/api/open-data', openDataRouter);
+app.use('/api/open-data', openDataRouter);
 // app.use('/api/external-api', externalAPIRouter);
 
 const port = 3000;
