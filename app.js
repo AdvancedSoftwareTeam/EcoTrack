cea29acaa9f87b3725c2d0099a4bb266a692ffea
yafanada <125304512+yafanada@users.noreply.github.com>
2023-12-25 20:38:06 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,36 +1,16 @@
-/* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
-const sessionConfig = require('./middlewares/sessionConfig'); // Import the session configuration module
-const Contribution = require('./models/Contribution');
-
 const app = express();
-app.use(express.json());
-app.use(sessionConfig); // Apply the session configuration
-Contribution.sync();
+const reportRoutes = require('./routes/reportRoutes');
 
-// Define all routers
-const userRouter = require('./routes/userRoutes');
-const dataRouter = require('./routes/dataRoutes');
-// const alertsRouter = require('./routes/alertsRoutes');
-// const reportsRouter = require('./routes/reportsRoutes');
-// const scoresRouter = require('./routes/scoresRoutes');
-// const resourcesRouter = require('./routes/resourcesRoutes');
-const openDataRouter = require('./routes/open-dataRoutes');
-// const externalAPIRouter = require('./routes/external-apiRoutes');
+// Other configurations and middleware...
 
-// Use the routers
-app.use('/api/users', userRouter);
-app.use('/api/data', dataRouter);
-// app.use('/api/alerts', alertsRouter);
-// app.use('/api/reports', reportsRouter);
-// app.use('/api/scores', scoresRouter);
-// app.use('/api/resources', resourcesRouter);
-app.use('/api/open-data', openDataRouter);
-// app.use('/api/external-api', externalAPIRouter);
+// Use reportRoutes
+app.use('/api', reportRoutes);
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+// Other routes...
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
-module.exports = app;
