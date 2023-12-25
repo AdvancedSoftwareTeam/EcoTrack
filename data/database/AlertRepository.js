4649828f009c56cfc79ebec3c8d86c5a6e5b6bea
Ahd Ghazal <ahdgh02@gmail.com>
2023-12-26 01:11:02 +0200

    db name = ecotrack - NO H' git push

--- data/database/AlertRepository.js
+++ data/database/AlertRepository.js
@@ -1,56 +1,56 @@
 const mysql = require('mysql2');
 const { notifyUser } = require('../../services/SocketService');
-let alertsMap={};
-let allAlerts=[];
+let alertsMap = {};
+let allAlerts = [];
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '12345678',
-  database: 'ECHOTRACK',
+  database: 'ecotrack',
 });
-class AlertRepository{
-    async getAllAlerts(){
-        db.query('SELECT * FROM alalerts',(error, results) => {
-            if (error) {
-              return res.status(500).json({ message: 'Internal server error.' });
-            }
-            for(let i of results){
-                if(!alertsMap.hasOwnProperty(i['UserID'])){
-                    alertsMap[i['UserID']]=[];
-                }
-                let alertss=alertsMap[i['UserID']];
-                alertss.push(i);
-                alertsMap[i['UserID']]=alertss;
-            }
-            allAlerts=results;
-          },)
-    }
-    checkAlerts(req,res){
-        const { DataType, DataValue } = req.body;
-        for(let i of allAlerts){
-            if(i['AlertType']===DataType){
-                if(i['AlertThresholds']<=DataValue){
-                    notifyUser(i['UserID'],`${DataType} has exceeded the limit ${i['AlertThresholds']},\n and the Value now is ${DataValue}`)
-                }
-            }
+class AlertRepository {
+  async getAllAlerts() {
+    db.query('SELECT * FROM alalerts', (error, results) => {
+      if (error) {
+        return res.status(500).json({ message: 'Internal server error.' });
+      }
+      for (let i of results) {
+        if (!alertsMap.hasOwnProperty(i['UserID'])) {
+          alertsMap[i['UserID']] = [];
         }
-    }
-    createAlert(req,res){
-        const {AlertThresholds,AlertType,AlertName,UserID}=req.body
-        db.query(
-            'INSERT INTO alalerts (UserID, AlertName, AlertType, AlertThresholds) VALUES (?, ?, ?, ?)',
-            [UserID, AlertName, AlertType, AlertThresholds],
-            (insertError) => {
-              if (insertError) {
-                console.log(insertError);
-
-                
-              }
-              
-            },
+        let alertss = alertsMap[i['UserID']];
+        alertss.push(i);
+        alertsMap[i['UserID']] = alertss;
+      }
+      allAlerts = results;
+    });
+  }
+  checkAlerts(req, res) {
+    const { DataType, DataValue } = req.body;
+    for (let i of allAlerts) {
+      if (i['AlertType'] === DataType) {
+        if (i['AlertThresholds'] <= DataValue) {
+          notifyUser(
+            i['UserID'],
+            `${DataType} has exceeded the limit ${i['AlertThresholds']},\n and the Value now is ${DataValue}`,
           );
-          this.getAllAlerts();
-          return res.status(200).json("added")
+        }
+      }
     }
+  }
+  createAlert(req, res) {
+    const { AlertThresholds, AlertType, AlertName, UserID } = req.body;
+    db.query(
+      'INSERT INTO alalerts (UserID, AlertName, AlertType, AlertThresholds) VALUES (?, ?, ?, ?)',
+      [UserID, AlertName, AlertType, AlertThresholds],
+      (insertError) => {
+        if (insertError) {
+          console.log(insertError);
+        }
+      },
+    );
+    this.getAllAlerts();
+    return res.status(200).json('added');
+  }
 }
-module.exports=AlertRepository;
+module.exports = AlertRepository;
--- data/database/DataRepository.js
+++ data/database/DataRepository.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '12345678',
-  database: 'ECHOTRACK',
+  database: 'ecotrack',
 });
 
 class DataRepository {
